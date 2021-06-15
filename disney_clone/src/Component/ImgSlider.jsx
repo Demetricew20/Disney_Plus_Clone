84734f4c16334ab5c74c4134aaea50d53e7ccb5d
DemetriceW20 <demetrice_williams@hotmail.com>
2021-06-15 14:51:05 -0800

    Built and styled carouse image.

--- disney_clone/src/Component/ImgSlider.jsx
+++ disney_clone/src/Component/ImgSlider.jsx
@@ -4,13 +4,49 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import sliderBadging from '../Assets/images/slider-badging.jpg';
+import sliderBadag from '../Assets/images/slider-badag.jpg';
+import sliderScale from '../Assets/images/slider-scale.jpg';
+import sliderWV from '../Assets/images/slider-scales.jpg';
 
 const Carousel = styled(Slider)`
+    margin-top: 20px;
 
+    ul li button {
+        &:before: {
+            font-size: 10px;
+            color: rgb(150, 158, 171);
+        }
+    }
+
+    li.slick-active button:before {
+        color: white;
+    }
+
+    .slick-list {
+        overflow: visible;
+    }
+
+    button{
+        z-index: 1;
+    }
 `;
 
 const Wrap = styled.div`
+    cursor: pointer;
 
+    img {
+        border: 4px solid transparent;
+        width: 100%;
+        height: 100%;
+        border-radius: 4px;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb( 0 0 0 / 73%) 0px 16px 10px -10px;
+        transition-duration: 500ms;
+
+        &:hover {
+            border: 4px solid rgb(249, 249, 249, .8);
+        }
+    }
 `;
 
 
@@ -31,7 +67,13 @@ function ImgSlider() {
                 <img src={sliderBadging} alt="Series Image" />
             </Wrap>
             <Wrap>
-                <img src={sliderBadging} alt="Series Image" />
+                <img src={sliderBadag} alt="Movie Image" />
+            </Wrap>
+            <Wrap>
+                <img src={sliderScale} alt="Series Image" />
+            </Wrap>
+            <Wrap>
+                <img src={sliderWV} alt="Series Image" />
             </Wrap>
             
         </Carousel>
