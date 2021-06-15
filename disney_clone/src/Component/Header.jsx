3cca942904b5022f3e8106e64227d7b042b37c8b
DemetriceW20 <demetrice_williams@hotmail.com>
2021-06-15 13:23:04 -0800

    Added images, videos, and Home component.

--- disney_clone/src/Component/Header.jsx
+++ disney_clone/src/Component/Header.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Nav = styled.div`
     height: 70px;
     background: #090b13;
-`
+`;
+
+const Logo = styled.img`
+    width: 80px;
+`;
+
+const NavMenu = styled.div`
+    
+`;
 
 function Header() {
     return (
-        <Container>
-            
-        </Container>
+        <Nav>
+            <Logo src="" alt="" />
+            <NavMenu>
+                
+            </NavMenu>
+        </Nav>
     )
 }
 
