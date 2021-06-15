43106ed7e432d3df50d1f19ea7848a3a6b56d4e4
DemetriceW20 <demetrice_williams@hotmail.com>
2021-06-15 13:14:11 -0800

    Created header component. Removed code that will not be utilized.

---
+++ disney_clone/src/Component/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 70px;
+    background: #090b13;
+`
+
+function Header() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Header
